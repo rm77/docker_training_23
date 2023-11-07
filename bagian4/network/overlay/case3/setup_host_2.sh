fa73d1bf59ebad0f5af46e1b4b1bdef3e13dfacc
roy <royyana@gmail.com>
2023-11-07 19:11:21 +0700

    change

--- bagian4/network/overlay/case3/setup_host_2.sh
+++ bagian4/network/overlay/case3/setup_host_2.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3j5mzy4klbg3mhenlgenjl1peyklrmmigbx3z6y9ceq0fex5do-1idqet10sgqua9csedsnnl7ym 10.21.84.254:2377
+    docker swarm join --token SWMTKN-1-2npcgu266ieeocfbv6274xddpeqxegmhz08oz9khzzprahc3mo-1zenrmxsclic4psba1k5ydlja 10.21.84.254:2377
