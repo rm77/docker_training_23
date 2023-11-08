3e0646504d45145d12b3913b75a21a32c6471e59
roy <royyana@gmail.com>
2023-11-08 16:22:30 +0700

    ch

--- bagian6/case2/build-and-push.sh
+++ bagian6/case2/build-and-push.sh
@@ -9,6 +9,6 @@ docker push 10.21.84.254:8443/mywebserver:2.1
 
 
 docker tag mywebserver:2.1 docker.io/royyana/mywebserver:2.1
-docker login -u royyana -p XXXXXXXXXXdckr_pat_UU8I4_8G_A39PqBQmTdCRkdZWEQ 
+docker login -u user1 -p qazwsx 
 docker push docker.io/royyana/mywebserver:2.1
 
