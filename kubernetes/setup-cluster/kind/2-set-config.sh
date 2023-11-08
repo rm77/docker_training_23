fcec5a7291feef7d063f5bc2f08bbddf02e35aaa
roy <royyana@gmail.com>
2023-11-08 07:25:10 +0700

    ok

--- kubernetes/setup-cluster/kind/2-set-config.sh
+++ kubernetes/setup-cluster/kind/2-set-config.sh
@@ -1,5 +1,4 @@
 kind export kubeconfig --name mylab99 --kubeconfig ./kubeconfig.cluster.yaml
 cp ./kubeconfig.cluster.yaml kubeconfig
 export KUBECONFIG=$(pwd)/kubeconfig
-source <(kubectl completion bash)
 kubectl get nodes
